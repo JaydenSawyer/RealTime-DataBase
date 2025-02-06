b1d42c08a68ffa929c94360e577114145b49cb77
Jayden Sawyer <jayden.sawyer25@d155.org>
2025-02-06 07:49:32 -0600

    Initial Commit

---
+++ RealTime DataBaseTests/RealTime_DataBaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealTime_DataBaseTests.swift
+//  RealTime DataBaseTests
+//
+//  Created by JAYDEN SAWYER on 2/6/25.
+//
+
+import Testing
+@testable import RealTime_DataBase
+
+struct RealTime_DataBaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ RealTime DataBaseUITests/RealTime_DataBaseUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RealTime_DataBaseUITestsLaunchTests.swift
+//  RealTime DataBaseUITests
+//
+//  Created by JAYDEN SAWYER on 2/6/25.
+//
+
+import XCTest
+
+final class RealTime_DataBaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
